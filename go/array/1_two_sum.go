90278c91f2ac508bd8165535b3c63638fda3810c
Mikail Khamkhoev <mkhamkhoev2004@gmail.com>
2026-03-11 00:56:49 +0300

    feat: add Two Sum solution in Go and Rust

---
+++ go/array/1_two_sum.go
@@ -0,0 +1,14 @@
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+package main
+
+func twoSum(nums []int, target int) []int {
+	m := make(map[int]int)
+	for i, num := range nums {
+		if idx, ok := m[target-num]; ok {
+			return []int{idx, i}
+		}
+		m[num] = i
+	}
+	return nil
+}
---
+++ rust/1_two_sum.rs
@@ -0,0 +1,14 @@
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut map = HashMap::new();
+        for (i, &num) in nums.iter().enumerate() {
+            if let Some(&idx) = map.get(&(target - num)) {
+                return vec![idx as i32, i as i32];
+            }
+            map.insert(num, i);
+        }
+        vec![]
+    }
+}
