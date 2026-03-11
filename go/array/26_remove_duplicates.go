9673b7e36fd15577407a7c28eeb78d67b80f7bbc
Mikail Khamkhoev <mkhamkhoev2004@gmail.com>
2026-03-12 00:01:56 +0300

    feat: add Remove Duplicates solution in Go

---
+++ go/array/26_remove_duplicates.go
@@ -0,0 +1,18 @@
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+package main
+
+func removeDuplicates(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+
+	slow := 0
+	for fast := 1; fast < len(nums); fast++ {
+		if nums[fast] != nums[slow] {
+			slow++
+			nums[slow] = nums[fast]
+		}
+	}
+	return slow + 1
+}
