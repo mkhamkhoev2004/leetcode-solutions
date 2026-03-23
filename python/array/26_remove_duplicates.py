50da1db4148ff9cbc8d3d91f4bc43f543e3de1f2
Mikail Khamkhoev <mkhamkhoev2004@gmail.com>
2026-03-23 20:29:38 +0300

    feat: add Remove Duplicates solution in Python

---
+++ python/array/26_remove_duplicates.py
@@ -0,0 +1,16 @@
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if len(nums) == 0:
+            return 0
+        
+        slow, fast = 0, 1
+        while fast < len(nums):
+            if nums[fast] != nums[slow]:
+                slow += 1
+                nums[slow] = nums[fast]
+            fast += 1
+        
+        return slow + 1
