3abb4453285406e4c7f3b717d6c11a573508b97b
Mikail Khamkhoev <mkhamkhoev2004@gmail.com>
2026-03-25 13:07:51 +0300

    feat: add necessary imports

--- python/array/35_search_insert_position.py
+++ python/array/35_search_insert_position.py
@@ -1,6 +1,9 @@
 # Time Complexity: O(log n)
 # Space Complexity: O(1)
 
+from typing import List
+
+
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
         left, right = 0, len(nums) - 1
