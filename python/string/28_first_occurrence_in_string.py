b43339c93434d7ba048260fb735a0ac2de73b9ee
Mikail Khamkhoev <mkhamkhoev2004@gmail.com>
2026-04-02 12:20:05 +0300

    feat: add First Occurrence In String solution in Python

---
+++ python/string/28_first_occurrence_in_string.py
@@ -0,0 +1,16 @@
+# Time Complexity: O(n * m)
+# Space Complexity: O(1)
+
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if not needle:
+            return 0
+        
+        if len(needle) > len(haystack):
+            return -1
+
+        for i in range(len(haystack) - len(needle) + 1):
+            if haystack[i:i + len(needle)] == needle:
+                return i
+
+        return -1
