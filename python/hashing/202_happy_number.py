f4614e6c2c1b37dd52c1d07f127c51e8675c06bd
Mikail Khamkhoev <mkhamkhoev2004@gmail.com>
2026-04-03 22:10:18 +0300

    feat: fix errors

--- python/hashing/202_happy_number.py
+++ python/hashing/202_happy_number.py
@@ -3,10 +3,10 @@
 
 class Solution:
     def isHappy(self, n: int) -> bool:
-        seen = {}
+        seen = set()
 
         while n != 1 and n not in seen:
             seen.add(n)
-            n = sum(int(digit**2 for digit in str(n)))
+            n = sum(int(digit)**2 for digit in str(n))
 
         return n == 1
