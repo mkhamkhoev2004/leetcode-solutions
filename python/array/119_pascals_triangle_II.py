e28cd052ac7bfe03bcb9fdaa6211495b01e5f31d
Mikail Khamkhoev <mkhamkhoev2004@gmail.com>
2026-04-05 14:27:28 +0300

    feat: add Pascal Triangle II soulution in Python

---
+++ python/array/119_pascals_triangle_II.py
@@ -0,0 +1,15 @@
+# Time Complexity: O(n^2)
+# Space Complexity: O(n)
+
+from typing import List
+
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        row = [0] * (rowIndex+1)
+        row[0] = 1
+
+        for i in range (1, rowIndex):
+            for j in range(i, 0, -1):
+                row[j] = row[j] + row[j-1]
+
+        return row
