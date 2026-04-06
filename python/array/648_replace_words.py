2428572fcd49c45588aad47b26d3cb1e0540f936
Mikail Khamkhoev <mkhamkhoev2004@gmail.com>
2026-04-06 19:44:17 +0300

    feat: add Replay Words solution in Python

---
+++ python/array/648_replace_words.py
@@ -0,0 +1,17 @@
+# Time Complexity: O(n * m)
+# Space Complexity: O(d + n)
+
+class Solution:
+    def replaceWords(self, dictionary: List[str], sentence: str) -> str:
+        root_set = set(dictionary)
+        words = sentence.split()
+
+        for i in range (len(words)):
+            for j in range(1, len(words[i]) + 1):
+                prefix = words[i][:j]
+                if prefix in root_set:
+                    words[i] = prefix
+                    break
+
+        
+        return " ".join(words)
