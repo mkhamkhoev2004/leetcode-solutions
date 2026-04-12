eee8c41ef1d37c1ce3470274714ad6bd37979b1a
Mikail Khamkhoev <mkhamkhoev2004@gmail.com>
2026-04-12 12:13:51 +0300

    feat: add Daily Temperature solution in Python

---
+++ python/array/739_daily_temperatures.py
@@ -0,0 +1,17 @@
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        stack = [0]
+        ans = [0] * len(temperatures)
+
+        for i in range(1, len(temperatures)):
+            
+            while stack and temperatures[stack[-1]] < temperatures[i]:
+                ans[stack[-1]] = i - stack[-1]
+                stack.pop()
+
+            stack.append(i)
+        
+        return ans
