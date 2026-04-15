fb731f4ca0db1aeeb156d8ed03874f6f0741fad2
Mikail Khamkhoev <mkhamkhoev2004@gmail.com>
2026-04-15 11:51:15 +0300

    feat: add Integer To Roman solution in Python

---
+++ python/hashing/12_integer_to_roman.py
@@ -0,0 +1,17 @@
+# Time Complexity: O(1)
+# Space Complexity: O(1)
+
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        val = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+        
+        symbols = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+        
+        res = ""
+        
+        for i in range(len(val)):
+            while num >= val[i]:
+                res += symbols[i]
+                num -= val[i]
+        
+        return res
